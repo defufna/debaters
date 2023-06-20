11bbcce69bd730f4660af32859d737b8a2096921
Ivan Savu <ivan.savu@gmail.com>
2023-06-20 16:04:25 +0200

    Spaces to tabs

--- src/server/Utils/LimitedHeap.cs
+++ src/server/Utils/LimitedHeap.cs
@@ -7,72 +7,72 @@ internal class LimitedHeap<T> : IEnumerable<T>
 {
 	int maxElements;
 	T[] items;
-    Comparison<T> comparison;
+	Comparison<T> comparison;
 
-    public int Count {get; private set;}
+	public int Count {get; private set;}
 
 	public LimitedHeap(int maxElements, Comparison<T> comparison)
-    {
-        this.maxElements = maxElements;
-        items = new T[maxElements];
-        Count = 0;
-        this.comparison = comparison;
-    }
+	{
+		this.maxElements = maxElements;
+		items = new T[maxElements];
+		Count = 0;
+		this.comparison = comparison;
+	}
 
-    public void Add(T item)
-    {
-        if(Count == maxElements)
-        {
-            if(comparison(items[0], item) < 0)
-            {
+	public void Add(T item)
+	{
+		if(Count == maxElements)
+		{
+			if(comparison(items[0], item) < 0)
+			{
 				items[0] = item;
 				FixTop();
 			}
 		}
-        else
-        {
-            items[Count++] = item;
+		else
+		{
+			items[Count++] = item;
 			FixBottom();
 		}
-    }
+	}
 
 	public bool TryGetTop(out T? top)
-    {
+	{
 		top = default(T);
 		if(Count == 0)
 			return false;
 
-        top = items[0];
+		top = items[0];
 		return true;
 	}
 
-    public T Remove()
-    {
-        if(Count == 0)
+	public T Remove()
+	{
+		if(Count == 0)
 			throw new InvalidOperationException("The heap is empty");
 
-        T item = items[0];
-        items[0] = items[Count - 1];
+		T item = items[0];
+		items[0] = items[Count - 1];
 		Count--;
 		FixTop();
-        return item;
+		return item;
 	}
 
-    public void Clear()
-    {
+	public void Clear()
+	{
 		Count = 0;
 	}
 
 	public IEnumerator<T> GetEnumerator()
-    {
-        for(int i = 0; i < Count; i++)
-            yield return items[i];
-    }
+	{
+		for(int i = 0; i < Count; i++)
+			yield return items[i];
+	}
 
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        return GetEnumerator();
-    }
+	IEnumerator IEnumerable.GetEnumerator()
+	{
+		return GetEnumerator();
+	}
 
 	private void FixBottom()
 	{
@@ -82,12 +82,12 @@ internal class LimitedHeap<T> : IEnumerable<T>
 		while(current != 0)
 		{
 			next = (current - 1) / 2;
-            if(comparison(items[current], items[next]) < 0)
-            {
+			if(comparison(items[current], items[next]) < 0)
+			{
 				Swap(current, next);
 				current = next;
 			}
-            else
+			else
 				break;
 
 		}
@@ -104,26 +104,26 @@ internal class LimitedHeap<T> : IEnumerable<T>
 			if (next >= Count)
 				break;
 
-            if (next + 1 < Count && comparison(items[next], items[next + 1]) > 0)
-            {
-                next++;
-            }
+			if (next + 1 < Count && comparison(items[next], items[next + 1]) > 0)
+			{
+				next++;
+			}
 
-            if (comparison(items[current], items[next]) <= 0)
-            {
-                break;
-            }
+			if (comparison(items[current], items[next]) <= 0)
+			{
+				break;
+			}
 
-            Swap(current, next);
-            current = next;
+			Swap(current, next);
+			current = next;
 		}
 	}
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	private void Swap(int first, int second)
 	{
-        T temp = items[first];
-        items[first] = items[second];
-        items[second] = temp;
+		T temp = items[first];
+		items[first] = items[second];
+		items[second] = temp;
 	}
 }
--- src/server/Utils/PasswordUtils.cs
+++ src/server/Utils/PasswordUtils.cs
@@ -10,12 +10,12 @@ internal class PasswordUtils
 	private const int SaltSize = 16;
 	private const string PepperEnvironmentVariable = "DEBATERS_PEPPER";
 	private static void GenerateSalt(Span<byte> salt)
-    {
-        using (var rng = RandomNumberGenerator.Create())
-        {
-            rng.GetBytes(salt);
-        }
-    }
+	{
+		using (var rng = RandomNumberGenerator.Create())
+		{
+			rng.GetBytes(salt);
+		}
+	}
 	public static byte[] HashPassword(string password)
 	{
 		byte[] passBytes = Encoding.UTF8.GetBytes(password);
@@ -43,42 +43,42 @@ internal class PasswordUtils
 		return CalculateEntropy(password) > RequiredEntropy;
 	}
 
-    public static double CalculateEntropy(string password)
-    {
-        int charsetSize = 0;
+	public static double CalculateEntropy(string password)
+	{
+		int charsetSize = 0;
 
-        bool hasLowerCase = false;
-        bool hasUpperCase = false;
-        bool hasNumber = false;
-        bool hasSpecialChar = false;
+		bool hasLowerCase = false;
+		bool hasUpperCase = false;
+		bool hasNumber = false;
+		bool hasSpecialChar = false;
 
-        foreach (char c in password)
-        {
-            if (char.IsLower(c) && !hasLowerCase)
-            {
-                charsetSize += 26; // Add lowercase letters
-                hasLowerCase = true;
-            }
-            else if (char.IsUpper(c) && !hasUpperCase)
-            {
-                charsetSize += 26; // Add uppercase letters
-                hasUpperCase = true;
-            }
-            else if (char.IsDigit(c) && !hasNumber)
-            {
-                charsetSize += 10; // Add numbers
-                hasNumber = true;
-            }
-            else if (!char.IsLetterOrDigit(c) && !hasSpecialChar)
-            {
-                charsetSize += 33; // Add special characters
-                hasSpecialChar = true;
-            }
-        }
+		foreach (char c in password)
+		{
+			if (char.IsLower(c) && !hasLowerCase)
+			{
+				charsetSize += 26; // Add lowercase letters
+				hasLowerCase = true;
+			}
+			else if (char.IsUpper(c) && !hasUpperCase)
+			{
+				charsetSize += 26; // Add uppercase letters
+				hasUpperCase = true;
+			}
+			else if (char.IsDigit(c) && !hasNumber)
+			{
+				charsetSize += 10; // Add numbers
+				hasNumber = true;
+			}
+			else if (!char.IsLetterOrDigit(c) && !hasSpecialChar)
+			{
+				charsetSize += 33; // Add special characters
+				hasSpecialChar = true;
+			}
+		}
 
-        double entropy = Math.Log(charsetSize, 2) * password.Length;
-        return entropy;
-    }
+		double entropy = Math.Log(charsetSize, 2) * password.Length;
+		return entropy;
+	}
 
 	private static void HashPasswordInternal(ReadOnlySpan<byte> password, ReadOnlySpan<byte> salt, Span<byte> result)
 	{
@@ -91,8 +91,8 @@ internal class PasswordUtils
 		sha256.GetHashAndReset(result);
 	}
 
-    private static byte[] GetPepper()
-    {
+	private static byte[] GetPepper()
+	{
 		string? pepper = Environment.GetEnvironmentVariable(PepperEnvironmentVariable);
 		if(pepper == null)
 		{
@@ -100,6 +100,6 @@ internal class PasswordUtils
 		}
 
 		return Encoding.UTF8.GetBytes(pepper);
-    }
+	}
 
 }
